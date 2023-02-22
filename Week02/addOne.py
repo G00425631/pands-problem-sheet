2dc269743586a3f12b7677818a53ca1a57b9c861
unknown <G00425631@atu.ie>
2023-02-22 15:10:10 +0000

    Week02

---
+++ Week02/addOne.py
@@ -0,0 +1,7 @@
+#addOne.py
+#This program adds 1 to a chosen number
+#Author: Dorina Agache
+
+number= int(input('Please enter a number:'))
+newnumber= number + 1
+print(f'{number} plus one is {newnumber}')
