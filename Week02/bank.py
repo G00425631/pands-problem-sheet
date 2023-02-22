02698e3015537723f2c1ed11c18937f9b168cd8e
unknown <G00425631@atu.ie>
2023-02-22 15:28:19 +0000

    Week02

---
+++ Week02/bank.py
@@ -0,0 +1,10 @@
+# bank.py
+# This program adds two sums of cents and converts into euros
+# Author:Dorina Agache
+
+cent1= int(input('Enter amount1 (in cent):'))
+cent2= int(input('Enter amount2 (in cent):'))
+
+sum= (cent1 +cent2)/100
+
+print(f'The sum of these is €{sum}')
