ef1e2d8359a91a34036c9d85b2f96e2445a8c152
unknown <G00425631@atu.ie>
2023-02-22 17:07:28 +0000

    bank.py

--- bank.py
+++ bank.py
@@ -1,10 +1,11 @@
 # bank.py
 # This program adds two sums of cents and converts into euros
 # Author:Dorina Agache
+#REF: FOR DECIMAL POINT https://www.w3schools.com/python/python_string_formatting.asp
 
 cent1= int(input('Enter amount1 (in cent):'))
 cent2= int(input('Enter amount2 (in cent):'))
 
 sum= (cent1 +cent2)/100
 
-print(f'The sum of these is €{sum}')
+print(f'The sum of these is €{sum:.2f}')
